1266b6223aead3ccee8ca907da2829670537fe99
Robocat <neatfx@gmail.com>
2019-09-16 19:48:29 +0800

    refactor: middleware

--- renderer-process/minidozer/Middleware.ts
+++ renderer-process/minidozer/Middleware.ts
@@ -12,49 +12,46 @@ interface MiddlewareParams<S> {
     reducer: Reducer<S>;
     suspend: (action: Action<object>, destroy?: boolean) => void;
 }
+interface Middleware {
+    <S>(params: MiddlewareParams<S>): Promise<void>;
+}
 interface Middlewares {
-    readonly internal: (<S>(params: MiddlewareParams<S>) => Promise<void>)[];
-    external: (<S>(params: MiddlewareParams<S>) => Promise<void>)[];
+    readonly internal: Middleware[];
+    external: Middleware[];
 }
 
-async function asyncActionMiddleware<S>(params: MiddlewareParams<S>): Promise<void> {
+const asyncActionMiddleware: Middleware = async params => {
     const { action } = params
 
     action.next = await Promise.resolve(action.prev)
 }
 
-async function suspenseMiddleware<S>(params: MiddlewareParams<S>): Promise<void> {
+const suspenseMiddleware: Middleware = async params => {
     const { suspend, action } = params
 
-    const act = action.next
-
-    if (act) {
-        if (act.status === ActionStatus.PENDING) {
-            act.status = ActionStatus.SUCCESS
-        }
-
-        if (act.status === ActionStatus.FAILED || act.response) {
-            suspend(act)
-        }
-
-        setTimeout((): void => {
-            suspend(act, true)
-        }, 500)
+    if (action.next.status === ActionStatus.PENDING) {
+        action.next.status = ActionStatus.SUCCESS
     }
+
+    if (action.next.status === ActionStatus.FAILED || action.next.response) {
+        suspend(action.next)
+    }
+
+    setTimeout((): void => {
+        suspend(action.next, true)
+    }, 500)
 }
 
-async function logMiddleware<S>(params: MiddlewareParams<S>): Promise<void> {
+const logMiddleware: Middleware = async params => {
     const { moduleName, state, reducer, action } = params
     const tracer = new Tracer('Minidozer.Dispatcher')
 
-    if (action) {
-        tracer.log('Action', {
-            'From': moduleName,
-            'Prev State': state,
-            'Action': action,
-            'Next State': reducer(state, action.next)
-        })
-    }
+    tracer.log('Action', {
+        'From': moduleName,
+        'Prev State': state,
+        'Action': action,
+        'Next State': reducer(state, action.next)
+    })
 }
 
 export const middlewares: Middlewares = {
